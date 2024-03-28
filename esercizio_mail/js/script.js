ebe985f7a928227fcccc2fe19cbf0387cfd2d67b
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-03-28 17:33:43 +0100

    fix: now working

--- esercizio_mail/js/script.js
+++ esercizio_mail/js/script.js
@@ -1,17 +1,28 @@
 const userMail = prompt ("Qual'è la tua mail?");
 const acceptedMails = ["pippo@mail.com", "pluto@mail.com", "topolino@mail.com"];
 
+// pippo@mail.com
+let flag = false;
 
-for (i = 0; i < acceptedMails.length; i++){
-    let flag = false;
-
-    if (flag = true){
-        console.log("Sei autorizzato ad accedere! " + userMail);
-    } else {
-        console.log("Non sei autorizzato!");
-    }
-
+for (i = 0; i < acceptedMails.length && flag == true; i++){
     if (userMail === acceptedMails [i]){
         flag = true;
+        // i = acceptedMails.length
     }
 }
+
+if (flag == true) {
+    console.log("Sei autorizzato ad accedere! " + userMail);
+} else {       
+    console.log("Non sei autorizzato!");
+}
+
+// Inizio a leggere la lista nome per nome 
+//     Se il nome è nella lista 
+//         segno che è nella lista
+
+// Se non l'ho segnato 
+//     lo caccio 
+// Altrimenti
+//     lo faccio entrare 
+
