e18fe0d2f186863ef2388e7dd9f8c647e95210af
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-03-28 18:18:45 +0100

    feat: ugly css

--- esercizio_dadi/js/script.js
+++ esercizio_dadi/js/script.js
@@ -1,14 +1,25 @@
+document.getElementById("start").addEventListener("click", function() {
+    let user = "";
+    let userNum = Math.floor(Math.random() * 7);
+    console.log(userNum);
+    let pcNum = Math.floor(Math.random() * 7);
+    console.log(pcNum);
+    
+    if (userNum > pcNum) {
+        user = `Hai vinto ti è uscito ${userNum} che è più alto di ${pcNum}!`;
+    }else if (userNum === pcNum) {
+        user = `Hai pareggiato al pc è uscito ${userNum} e a te ${pcNum}!`
+    }else {
+        user = `Hai perso al pc è uscito ${pcNum} che è più alto di ${userNum}!`;
+    }
+    console.log(user);
 
-let user = "";
-let pc = "";
-let userNum = Math.floor(Math.random() * 7);
-console.log(userNum);
-let pcNum = Math.floor(Math.random() * 7);
-console.log(pcNum);
+    const divElem = document.getElementById("page"); 
+    console.log(divElem);
+    divElem.classList.add("hide")
 
-if (userNum > pcNum) {
-    user = `Hai vinto ti è uscito ${userNum} che è più alto di ${pcNum}`;
-}else {
-    user = `Hai perso al pc è uscito ${pcNum} che è più alto di ${userNum}`;
-}
-console.log(user);
+    const bodyElem = document.getElementById("resultPage")
+    console.log(bodyElem);
+    bodyElem.append(`${user}`)
+})
+
